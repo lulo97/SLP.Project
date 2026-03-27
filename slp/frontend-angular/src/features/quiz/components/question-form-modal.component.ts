e7ca33722d3cbf79ac842b4521bdba577703eb60
luongpysl2 <luongpysl2@gmail.com>
2026-03-28 06:39:21 +0700

    fix question form modal

--- slp/frontend-angular/src/features/quiz/components/question-form-modal.component.ts
+++ slp/frontend-angular/src/features/quiz/components/question-form-modal.component.ts
@@ -1,30 +1,36 @@
-
-
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NzModalModule } from 'ng-zorro-antd/modal';
-import { QuestionFormComponent } from '../../question/components/question-form.component';
-import { CreateQuestionPayload } from '../../question/question.model';
-import { DisplayQuestion } from '../quiz.model';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NzModalModule } from "ng-zorro-antd/modal";
+import { QuestionFormComponent } from "../../question/components/question-form.component";
+import { CreateQuestionPayload } from "../../question/question.model";
+import { DisplayQuestion } from "../quiz.model";
+import { TranslateService, TranslateModule } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-question-form-modal',
+  selector: "app-question-form-modal",
   standalone: true,
-  imports: [CommonModule, NzModalModule, QuestionFormComponent],
+  imports: [
+    CommonModule,
+    NzModalModule,
+    QuestionFormComponent,
+    TranslateModule,
+  ],
   template: `
     <nz-modal
       [(nzVisible)]="visible"
-      [nzTitle]="title"
+      [nzTitle]="modalTitle"
       [nzFooter]="null"
       [nzWidth]="800"
       (nzOnCancel)="handleCancel()"
     >
-      <app-question-form
-        [initialQuestion]="initialQuestion"
-        [loading]="loading"
-        (save)="handleSave($event)"
-        (cancel)="handleCancel()"
-      ></app-question-form>
+      <ng-template nzModalContent>
+        <app-question-form
+          [initialQuestion]="initialQuestion"
+          [loading]="loading"
+          (save)="handleSave($event)"
+          (cancel)="handleCancel()"
+        ></app-question-form>
+      </ng-template>
     </nz-modal>
   `,
 })
@@ -35,6 +41,14 @@ export class QuestionFormModalComponent {
   @Output() visibleChange = new EventEmitter<boolean>();
   @Output() saved = new EventEmitter<CreateQuestionPayload>();
 
+  constructor(private translate: TranslateService) {}
+
+  get modalTitle(): string {
+    return this.question
+      ? this.translate.instant("quiz.updateQuestion")
+      : this.translate.instant("quiz.createQuestion");
+  }
+
   get initialQuestion(): any {
     if (!this.question) return null;
     return {
@@ -42,13 +56,11 @@ export class QuestionFormModalComponent {
       content: this.question.content,
       type: this.question.type,
       explanation: this.question.explanation,
-      metadataJson: this.question.metadata ? JSON.stringify(this.question.metadata) : undefined,
+      metadataJson: this.question.metadata
+        ? JSON.stringify(this.question.metadata)
+        : undefined,
       tags: this.question.tags,
     };
-  }
-
-  get title(): string {
-    return this.question ? 'quiz.editQuestion' : 'quiz.createQuestion';
   }
 
   handleSave(payload: CreateQuestionPayload): void {
